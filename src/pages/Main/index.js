2c0381d5223b261692636354ca83b20a41a95e05
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2020-01-26 16:53:19 -0300

    First commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React            from 'react';
+import { Container }    from './styles';
+
+export default function Main() {
+  return (
+        <Container>
+          
+        </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+}
