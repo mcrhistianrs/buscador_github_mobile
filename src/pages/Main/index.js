68d6266367c5f2415f8dd5025196f77ed560d0dd
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2020-01-29 05:55:51 -0300

    Estilizando listagem-comecando

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,56 @@
-import React            from 'react';
-import { Container }    from './styles';
+import React, { Component }                           from 'react';
+import {Keyboard}                                     from 'react-native';
+import Icon                                           from 'react-native-vector-icons/MaterialIcons';
+import api                                            from '../../services/api';
+import { Container, Form , Input , SubmitButtom}      from './styles';
+import { Picker } from 'react-native';
 
-export default function Main() {
-  return (
+export default class  Main extends Component {
+
+  state = {
+    newUser: '',
+    user:[]
+  }
+
+  handleAddUser = async () => {
+    const { users, newUser} = this.state;
+    const response = await api.get(`/users/${newUser}`);
+    const data = {
+      name    : response.data.name,
+      login   : response.data.login,
+      bio     : response.data.bio,
+      avatar  : response.data.avatar_url,
+    };
+
+    this.setState({
+      users       :  [...users, data],
+      newUser     : '',
+    });
+
+    Keyboard.dismiss();
+  }
+
+  render(){
+    const { users,newUser} = this.state;
+      return (
         <Container>
-          
+          <Form>
+            <Input 
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Adicionar Usuário"
+              onChangeText={text => this.setState({newUser: text})}
+              returnKeyType="send"
+              onSubmitEditing={this.handleAddUser}
+            />
+            <SubmitButtom onPress={this.handleAddUser}>
+              <Icon name="add" size={20} color="#FFF" />
+          </SubmitButtom>
+          </Form>
+        
         </Container>
-  );
+      );
+  }
 }
 
 Main.navigationOptions = {
