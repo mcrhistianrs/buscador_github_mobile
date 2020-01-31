4f10dd1fd52908e5d795b603df298bc2f941f355
Maicon Crhistian <maiconcrhistian@MacBook-Pro-de-Maicon.local>
2020-01-31 19:39:31 -0300

    realizando navegacao,comecando

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component }                                 from 'react';
 import {Keyboard , ActivityIndicator}                       from 'react-native';
+import AsyncStorage                                         from '@react-native-community/async-storage';
 import Icon                                                 from 'react-native-vector-icons/MaterialIcons';
 import api                                                  from '../../services/api';
 import { Container,
@@ -13,7 +14,7 @@ import { Container,
    Bio,
    ProfileButtom,
    ProfileButtomText }      from './styles';
-import { Picker } from 'react-native';
+
 
 export default class  Main extends Component {
 
@@ -21,6 +22,21 @@ export default class  Main extends Component {
     newUser: '',
     users:[],
     loading:false
+  }
+
+
+  async componentDidMount(){
+    const users = await AsyncStorage.getItem('users');
+    if (users){
+      this.setState({users: JSON.parse(users)});
+    }
+  }
+
+   componentDidUpdate(_,prevState){
+    const { users } = this.state;
+    if ( prevState.users != users ){
+         AsyncStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   handleAddUser = async () => {
